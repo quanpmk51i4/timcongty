912c2d73318fd84b9cb18c96f2c9279f7c3f9d1b
quanpmk51i4 <126937572+quanpmk51i4@users.noreply.github.com>
2023-03-09 20:00:04 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tìm kiếm công ty</title>
+</head>
+<body>
+	<h1>Tìm kiếm công ty</h1>
+<form method="get" action="https://timcongty.herokuapp.com/search.php">
+		<label for="company_name">Tên công ty:</label>
+		<input type="text" name="company_name" id="company_name" required>
+		<button type="submit">Tìm kiếm</button>
+	</form>
+</body>
+</html>
