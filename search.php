6d188fd5ef67f7c73150e69bb6c343187993dce3
quanpmk51i4 <126937572+quanpmk51i4@users.noreply.github.com>
2023-03-09 20:48:17 +0700

    Update search.php

--- search.php
+++ search.php
@@ -1,32 +1,57 @@
 <?php
 
-// Lấy tên công ty từ biến GET
-$company_name = $_GET['company_name'];
+if (isset($_POST['company_name'])) {
+    $company_name = $_POST['company_name'];
+    $prompt = "Tell me about $company_name";
+    $url = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+    $data = array(
+        'prompt' => $prompt,
+        'max_tokens' => 100,
+        'n' => 1,
+        'stop' => '.'
+    );
+    $payload = json_encode($data);
+    $headers = array(
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $_ENV['OPENAI_API_KEY']
+    );
 
-// API key của bạn
-$api_key = getenv("OPENAI_API_KEY");
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $payload,
+        CURLOPT_HTTPHEADER => $headers
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
 
-// Thực hiện yêu cầu API để tìm kiếm thông tin công ty
-$url = "https://api.openai.com/v1/models";
-$data = array(
-    "documents" => array("List of companies and their descriptions"),
-    "query" => $company_name,
-    "max_tokens" => 50,
-    "temperature" => 0.5,
-    "stop" => "."
-);
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/json\r\nAuthorization: Bearer ".$api_key,
-        'method'  => 'POST',
-        'content' => json_encode($data),
-    ),
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-
-// Hiển thị kết quả trên trang
-echo "<h1>Kết quả tìm kiếm cho '$company_name':</h1>";
-echo "<p>" . $result . "</p>";
-
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        $data = json_decode($response, true);
+        $text = $data['choices'][0]['text'];
+        echo $text;
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Company Search</title>
+</head>
+<body>
+    <form method="POST">
+        <label for="company_name">Company Name:</label>
+        <input type="text" name="company_name" id="company_name">
+        <button type="submit">Search</button>
+    </form>
+</body>
+</html>
